0be0adb50bdbd07a7118b8b11b7c7760d08e0c6a
Pushpendra23 <pushpendrasinghrathore.it18@gmail.com>
2017-06-14 22:58:04 +0530

    bigdata day 1

--- menu_for_cluster_setup.py
+++ menu_for_cluster_setup.py
@@ -8,6 +8,10 @@ import os, commands,time
 #Forming the list of all available machines and storing in Fetched_ip list
 Fetched_ip=[]
 
+
+
+#CODE FOR ADHOC LAB
+"""
 #Taking range from 100 to 120 ip only with predefined subnet 255.255.255.0
 for i in range(200)[100:120]:
 
@@ -17,6 +21,19 @@ for i in range(200)[100:120]:
 	#Acessing only the first element of list and storing it in Fetched_ip
 	if check[0]==0:
 		Fetched_ip.append("192.168.10."+str(i))
+"""
+
+#CODE FOR HOME ENVIORNMENT
+Look_ip=[48]
+for i in Look_ip:
+	#Check is temporary variable to store command output
+	check=commands.getstatusoutput('ping -c 1 192.168.122.'+str(i))
+
+	#Acessing only the first element of list and storing it in Fetched_ip
+	if check[0]==0:
+		Fetched_ip.append("192.168.122."+str(i))
+
+
 
 #The list Final_Table stores the ip,core and RAM of machine
 Final_Table=[]
@@ -25,7 +42,7 @@ Final_Table=[]
 for i in Fetched_ip:
 
 	#Making connection through ssh to get cpu and core info
-	commands.getoutput("ssh root@192.168.10."+i)
+	commands.getoutput('ssh root@192.168.122.'+str(i))
 	#ram_size store the available ram 	
 	ram_size=commands.getoutput("cat /proc/meminfo | grep -i memtotal | cut -d: -f2")
 
--- start.py
+++ start.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python
+
+#First script that will run...
+
+#Importing the basis modules
 import os, commands,time
+
+#Variable to print message
 msg="""
-Hello hold ursewlf tight ...
-you are entering in the world of data.
+Hello hold yourself tight ...
+
+                              You are entering in the world of data.
 
 _____________________________________
 .....................................
@@ -12,14 +19,17 @@ Press 1 for manual setup
 Press 2 for automatic setup
 """
 print msg
-#choice variable store the desired option choosen by user
 
+#choice variable store the desired option choosen by user
 choice=raw_input()
 if choice=='1':
-	print "Grate one should do and own completly what he is doing..."
-	execfile('menu.py')
+	print "Great one should do and own completly what he is doing..."
+#link to open menu_for_cluster_setup.py
+	execfile('menu_for_cluster_setup.py')
 elif choice=='2':
 	print "You prefer automation and for sure love technology..."
-	execfile("menu.py")
+#link to open auto_cluster_setup.py
+	execfile("auto_cluster_setup.py")
 else:
+#In case if wrong input is given
 	print "hey man check your choice again"
